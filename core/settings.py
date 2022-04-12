f54e0efaf3b04fef2cd7555a1089c49c754faa5b
Daniel Linder <49314224+Xeranif@users.noreply.github.com>
2022-04-12 01:56:14 -0500

    Refactoring of the Store Templates
    Updated the Store, Basket, Account, and Payment templates
    
    Co-Authored-By: Barbara Woods <98857275+vaniiityb@users.noreply.github.com>

--- core/settings.py
+++ core/settings.py
@@ -98,14 +98,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 #Basket session ID
 BASKET_SESSION_ID =  'basket'
 
-<<<<<<< HEAD
-# Stripe Payment
-os.environ.setdefault('STRIPE_PUBLISHABLE_KEY', 'pk_test_51KmOs7Be5RFpBieIhkyQzobvgqEFbACt5Z1fq17xCTxu8XyvNkhAy7mKSGCAfVVVvgzeK3ulmFKTQppLTEsibZ2T00TJtuENFq')
-STRIPE_SECRET_KEY = 'sk_test_51KmOs7Be5RFpBieIuySBL0iZR7NdxAAx0OZsOLD9d3cYbcftbvlJcQBHlI644oylELRudCh8FeirDl9UxqSvPev500NH5Ywe40'
-# stripe listen --forward-to localhost:8000/payment/webhook/
-=======
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
->>>>>>> parent of d22198b (commit to dev3)
 
 # Custom user model
 AUTH_USER_MODEL = 'account.UserBase'
@@ -116,7 +109,6 @@ LOGIN_URL = '/account/login/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Stripe Payment
-PUBLISHABLE_KEY = 'pk_test_51KmOs7Be5RFpBieIhkyQzobvgqEFbACt5Z1fq17xCTxu8XyvNkhAy7mKSGCAfVVVvgzeK3ulmFKTQppLTEsibZ2T00TJtuENFq'
-SECRET_KEY = 'sk_test_51KmOs7Be5RFpBieIuySBL0iZR7NdxAAx0OZsOLD9d3cYbcftbvlJcQBHlI644oylELRudCh8FeirDl9UxqSvPev500NH5Ywe40'
-STRIPE_ENDPOINT_SECRET = 'whsec_51cb271ab71344e2cecb25bd8266d806af1d472f6a19e69c595c00a9a419742c'
+os.environ.setdefault('STRIPE_PUBLISHABLE_KEY', 'pk_test_51KmOs7Be5RFpBieIhkyQzobvgqEFbACt5Z1fq17xCTxu8XyvNkhAy7mKSGCAfVVVvgzeK3ulmFKTQppLTEsibZ2T00TJtuENFq')
+STRIPE_SECRET_KEY = 'sk_test_51KmOs7Be5RFpBieIuySBL0iZR7NdxAAx0OZsOLD9d3cYbcftbvlJcQBHlI644oylELRudCh8FeirDl9UxqSvPev500NH5Ywe40'
 # stripe listen --forward-to localhost:8000/payment/webhook/
